23811aa2b34ffc818732810dbd218535bbf2bf93
Liliia Galkina <vvlili@yandex.ru>
2026-02-23 22:17:37 +0300

    header адаптив

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,30 @@
 import style from "./header.module.scss";
 import ButtonBorder from "../ButtonBorder/ButtonBorder";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-	  <header className={style.header}>
-		  <img src="/images/home/header-decor-up.png" alt="multicolored abstractive" className={style.headerdecor } />
+    <header className={style.header}>
+      <img
+        src="/images/home/header-decor-up.png"
+        alt="multicolored abstractive"
+        className={style.headerdecor}
+      />
       <div className="container">
         <div className={style.items}>
           <a href="#" className={style.logo}>
             <img src="./images/logo.png" alt="logo" />
           </a>
           <nav className={style.menu}>
-            <ul className={style.menulist}>
+            <div
+             className={isMenuOpen ? style.burgeractive : style.burger}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              <span></span>
+            </div>
+            <ul className={isMenuOpen ? style.menulistactive : style.menulist}>
               <li className={style.menuitem}>
                 <a href="#" className={style.menulink}>
                   Home
