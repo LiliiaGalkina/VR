d118f991637a812bf13e942236a3d604d0c5f09e
Liliia Galkina <vvlili@yandex.ru>
2026-03-06 18:30:35 +0300

    contactuspage с адаптивом

--- src/components/ContactUs/ContactUsPage.tsx
+++ src/components/ContactUs/ContactUsPage.tsx
@@ -2,17 +2,32 @@ import style from "./contactuspage.module.scss";
 import TitleBlock from "../TitleBlock/TitleBlock";
 import ContactItems from "./ContactItems/ContactItems";
 import GetInTouch from "./GetInTouch/GetInTouch";
+import Subscribe from "../Subscribe/Subscribe";
 
 const ContactUsPage = () => {
-    return (
-        <main>
-            <div className="container">
-                <TitleBlock titletext="Contact Us"/>
-                <ContactItems/>
-                <GetInTouch/>
+  return (
+    <main>
+      <div className="container">
+        <TitleBlock titletext="Contact Us" />
+        <ContactItems />
+        <GetInTouch />
+        <div className={style.mapblock}>
+          <div className={style.map}>
+            <img src="/images/contactus/map.png" className={style.mapimg} alt="map" />
+            <div className={style.location}>
+              <div className={style.inner}>
+                <img
+                  src="/images/contactus/icon-location.svg"
+                  alt="location icon"
+                />
+              </div>
             </div>
-        </main>
-    )
-}
+          </div>
+			  </div>
+			  <Subscribe/>
+      </div>
+    </main>
+  );
+};
 
 export default ContactUsPage;
