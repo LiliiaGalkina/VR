7b7e822dd52cc9c61ecbc90acec37dcab200af01
Liliia Galkina <vvlili@yandex.ru>
2026-03-08 20:00:54 +0300

    испр расчета блоков в блоге

--- src/components/BlogPageItems/BlogPageItems.tsx
+++ src/components/BlogPageItems/BlogPageItems.tsx
@@ -4,40 +4,38 @@ import ReactPaginate from "react-paginate";
 import { useEffect, useState } from "react";
 import type { IBlogItems } from "../../types";
 import BlogPageItem from "./BlogPageItem";
+import useResponsiveEvent from "../../useResponsiveEvent";
 
 const BlogPageItems = () => {
   const [page, setPage] = useState(0);
   const [filterData, setFilterData] = useState<IBlogItems[]>([]);
-  const n = 9;
+
+  const isLaptop = useResponsiveEvent(1119);
+  let count = isLaptop ? 10 : 9;
 
   useEffect(() => {
     return setFilterData(
       blogItems.filter((_, index) => {
-        return index >= page * n && index < (page + 1) * n;
+        return index >= page * count && index < (page + 1) * count;
       }),
     );
-  }, [page]);
+  }, [page, count]);
 
   return (
     <>
       <div className={style.items}>
         {filterData &&
-          filterData.map((item) => (
-            <BlogPageItem
-              key={item.id} {...item}
-            />
-          ))}
+          filterData.map((item) => <BlogPageItem key={item.id} {...item} />)}
       </div>
       <ReactPaginate
         containerClassName={style.pagination}
         pageClassName={style.pageitem}
         activeClassName={style.active}
         onPageChange={(event) => setPage(event.selected)}
-        pageCount={Math.ceil(blogItems.length / n)}
+        pageCount={Math.ceil(blogItems.length / count)}
         previousLabel={""}
         nextLabel={""}
       />
-      
     </>
   );
 };
