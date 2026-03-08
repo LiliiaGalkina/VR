fc33a15c9ad7f50a7df848b0632ac02a8d6f094d
Liliia Galkina <vvlili@yandex.ru>
2026-03-08 17:31:11 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,14 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import AppRouter from "./AppRouter";
-import { BrowserRouter as Router } from "react-router-dom";
-import ScrollToTop from "./ScrollToTop";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <ScrollToTop/>
-        <Header />
-        <AppRouter />
-        <Footer />
-
+      <Header />
+      <AppRouter />
+      <Footer />
     </Router>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://liliiagalkina.github.io/VR/",
   plugins: [react()],
+  base: "https://liliiagalkina.github.io/VR/"
 });
