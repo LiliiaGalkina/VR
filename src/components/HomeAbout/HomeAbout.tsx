3bf4a2cf6b40d72144d68807d2020b1e65a51173
Liliia Galkina <vvlili@yandex.ru>
2026-03-21 17:48:15 +0300

    изменение верстки

--- src/components/HomeAbout/HomeAbout.tsx
+++ src/components/HomeAbout/HomeAbout.tsx
@@ -2,7 +2,7 @@ import style from "./homeabout.module.scss";
 import ButtonColor from "../ButtonColor/ButtonColor";
 import ImagesBlock from "../ImagesBlock/ImagesBlock";
 import { Link } from "react-router-dom";
-import {useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 import { itemsDownUP } from "../../helpers";
 import { leftToRight } from "../../helpers";
 import { gsap } from "gsap/gsap-core";
@@ -11,10 +11,10 @@ const HomeAbout = () => {
   const contentRef = useRef(null);
 
   useLayoutEffect(() => {
-	  itemsDownUP(contentRef.current);
-	    return () => {
-        gsap.killTweensOf(contentRef.current);
-      };
+    itemsDownUP(contentRef.current);
+    return () => {
+      gsap.killTweensOf(contentRef.current);
+    };
   }, []);
 
   return (
@@ -26,8 +26,8 @@ const HomeAbout = () => {
           video="./images/home/about-video.png"
           videoalt="virtual reality glasses"
           light="./images/home/about-main-light.png"
-				  bg="./images/home/about-main-bg.png"
-				  gsapfunction={leftToRight}
+          bg="./images/home/about-main-bg.png"
+          gsapfunction={leftToRight}
         />
         <div className={style.content} ref={contentRef}>
           <h3 className={style.subtitle}>ABOUT US</h3>
