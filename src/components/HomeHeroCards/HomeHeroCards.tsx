d2e71ec8f2bd7992d87cb84708091b6106d1ce62
Liliia Galkina <vvlili@yandex.ru>
2026-03-24 20:25:58 +0300

    Update HomeHeroCards.tsx

--- src/components/HomeHeroCards/HomeHeroCards.tsx
+++ src/components/HomeHeroCards/HomeHeroCards.tsx
@@ -1,27 +1,19 @@
 import style from "./homeherocards.module.scss";
 import { heroItems } from "../../data";
-import { useEffect, useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 import { downToUp } from "../../helpers";
 import { gsap } from "gsap/gsap-core";
-import useResponsiveEvent from "../../useResponsiveEvent";
 
 const HomeHeroCards = () => {
 	const blockRef = useRef(null);
 
-     const isDesktop = useResponsiveEvent(1024);
-
-        const manageAnimations = (shouldAnimate: boolean) => {
-          gsap.killTweensOf([blockRef.current]);
-          if (shouldAnimate) {
-           downToUp(blockRef.current);
-          }
-        };
+	useLayoutEffect(() => {
+		downToUp(blockRef.current);
+		  return () => {
+        gsap.killTweensOf(blockRef.current);
+      };
+	}, [])
      
-        useEffect(() => {
-          manageAnimations(isDesktop);
-        }, [isDesktop]);
-     
-
     return (
         <div className={style.cards} ref={blockRef}>
             {heroItems.map((heroItem) => (
