0dee7a319865d782b9c1c98761e7b7876d11a7cb
Liliia Galkina <vvlili@yandex.ru>
2026-03-24 20:44:54 +0300

    изменение анимации

--- src/components/WhyChooseContentBlock/WhyChooseContentBlock.tsx
+++ src/components/WhyChooseContentBlock/WhyChooseContentBlock.tsx
@@ -1,29 +1,31 @@
 import style from "./whychoosecontentblock.module.scss";
 import WhyChooseContentItem from "./WhyChooseContentItem";
 import { whyChooseItems } from "../../data";
-import {useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 import { downToUp } from "../../helpers";
 import { gsap } from "gsap/gsap-core";
 
 const WhyChooseContentBlock = () => {
-	const contentRef = useRef(null);
+  const contentRef = useRef(null);
 
-	useEffect(() => {
-		downToUp(contentRef.current)
-	}, [])
+  useLayoutEffect(() => {
+    downToUp(contentRef.current);
+    return () => {
+      gsap.killTweensOf(contentRef.current);
+    };
+  }, []);
 
-	return (
+  return (
     <div className={style.content} ref={contentRef}>
       <h3 className={style.subtitle}>WHY CHOOSE US</h3>
       <h2 className={style.title}>Why Choose Us for Your VR Needs</h2>
       <div className={style.block}>
-       {whyChooseItems.map((item) => (
-        <WhyChooseContentItem key={item.id} {...item}/>
-       ))}
-       
+        {whyChooseItems.map((item) => (
+          <WhyChooseContentItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default WhyChooseContentBlock;
